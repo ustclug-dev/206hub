bca231c7fcb13808e34b98764f2dd9e9bf9ae851
taoky <taoky1999@gmail.com>
2020-01-16 17:06:19 +0800

    add commenters link

--- _plugins/archives.rb
+++ _plugins/archives.rb
@@ -14,16 +14,41 @@ module Jekyll
         hash.each_value { |posts| posts.sort!.reverse!.uniq! }
       end
     end
+
+    def commenters
+      begin
+        hash = Hash.new { |h, k| h[k] = [] }
+        collections.each do |name, collection|
+          collection.docs.each do |doc|
+            doc.data["comments"]&.each do |c|
+              hash[c["commenter"]] << doc
+            end
+          end
+        end
+        hash.each_value { |posts| posts.sort!.reverse!.uniq! }
+      end
+    end
   end
 
   module Archives
     class Archives
+      def read
+        read_tags
+        read_categories
+        read_dates
+        read_commenters
+      end
+
       def tags
         @site.tags
       end
 
       def categories
         @site.collections
+      end
+
+      def commenters
+        @site.commenters
       end
 
       def read_categories
@@ -34,6 +59,15 @@ module Jekyll
             end
             # Jekyll.logger.warn "COLLECTIONS: title: #{title}, posts: #{posts.docs}"
             @archives << Archive.new(@site, title, "category", posts.docs)
+          end
+        end
+      end
+
+      def read_commenters
+        if enabled? "commenters"
+          Jekyll.logger.warn "#{commenters}"
+          commenters.each do |title, posts|
+            @archives << Archive.new(@site, title, "commenter", posts)
           end
         end
       end
